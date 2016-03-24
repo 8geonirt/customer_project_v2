a52e0fef942aa22e34270d4e6726b9925b84b0c7
Jose Trinidad Espinoza Gonzalez <trinoeg8@gmail.com>
2016-03-24 15:47:14 -0600

    Employees test updated

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,11 @@
+# Employee documentation:
+#
 class Employee < ActiveRecord::Base
+
+  validates :first_name, presence: true, length: { minimum: 3, maximum: 100}
+  validates :last_name, presence: true, length: { minimum: 3, maximum: 100}
+  validates :address, presence: true, length: { minimum: 3, maximun: 100}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
 end
---
+++ test/models/employees_test.rb
@@ -0,0 +1,70 @@
+require 'test_helper'
+
+class Employee_Test < ActiveSupport::TestCase
+
+  def setup
+    @employee = Employee.new(first_name:"Jose", last_name:"Espinoza",address:"Mi calle", email:"trinoeg8@gmail.com",phone_number:"3121078193",activated:false)
+  end
+
+  test 'Employee should be valid' do
+    assert @employee.valid?, "Invalid employee"
+  end
+
+  test 'Employee first name should not be empty' do
+    @employee.first_name = " "
+    assert_not @employee.valid?
+  end
+
+  test 'Employee first name should not be too short' do
+    @employee.first_name = "as"
+    assert @employee.first_name.size < 3, 'First name length should be greater or equal than 3'
+  end
+
+  test 'Employee first name should not be too long' do
+    @employee.first_name = "a" * 101
+    assert @employee.first_name.size > 100, 'First name length should be less or equal than 100'
+    assert_operator @employee.first_name.size, :>, 100
+  end
+
+  test 'Employee last name should not be empty' do
+    @employee.last_name = ""
+    assert_not @employee.valid?
+  end
+
+  test 'Employee last name should not be too short' do
+    @employee.last_name = "as"
+    assert @employee.last_name.size < 3, "Last name length should be greater or equal than 3"
+  end
+
+  test 'Employee last name should not be too long' do
+    @employee.last_name = "a" * 101
+    assert @employee.last_name.size > 100, 'Last name length should be less or equal than 100'
+  end
+
+  test 'Address should not be empty' do
+    @employee.address = ""
+    assert_not @employee.valid?, 'Address should not be empty'
+  end
+
+  test 'Adress should not be too long' do
+    @employee.address = "a" * 101
+    assert @employee.address.size > 100, 'Address should not be too long'
+  end
+
+  test 'Email address should not be empty' do
+    @employee.email = ""
+    assert_not @employee.valid?, 'Email should not be empty'
+  end
+
+  test 'Email address should be valid' do
+    @employee.email = "bad@correo"
+    assert_not @employee.valid?, 'Email format must be correct'
+  end
+
+  test 'Email address should be unique' do
+    @employee.save
+    empl2 = @employee.dup
+    assert_not empl2.valid?, 'Email must be unique'
+  end
+
+end
