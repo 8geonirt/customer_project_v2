cabd6239b6006811c9d1331f85162cd988ed1cba
Jose Trinidad Espinoza Gonzalez <trinoeg8@gmail.com>
2016-04-11 18:05:13 -0500

    Create admin role in Employees Table

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require bootstrap-sprockets
 //= require dataTables/jquery.dataTables
 //= require bootstrap-datepicker/core
 //= require moment
